44e2a988b1c6dec1498053d65d5cc4386c17f19a
Tommi Savikko <tommi.savikko@skydivejkl.fi>
2015-05-23 19:04:49 +0300

    fix navbar collapse when clicking item

--- client/_main.js
+++ client/_main.js
@@ -3,3 +3,9 @@ Meteor.subscribe('dropzones');
 Meteor.subscribe('vtr');
 
 Meteor.subscribe('files');
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
