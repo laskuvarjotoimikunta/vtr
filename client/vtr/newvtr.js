7cf121f75f8e00921cd413537e3f40df1310f3ce
Tommi Savikko <tommi.savikko@skydivejkl.fi>
2015-05-25 15:48:00 +0300

    more intelligent date field

--- client/vtr/newvtr.js
+++ client/vtr/newvtr.js
@@ -8,3 +8,32 @@ AutoForm.hooks({
     }, 
   }
 });
+
+Template.newVtr.events({
+    'click #today': function(event) {
+        event.stopPropagation();
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1;
+        var yyyy = today.getFullYear();
+        if(dd<10){dd='0'+dd} 
+        if(mm<10){mm='0'+mm} 
+        today = yyyy+'-'+mm+'-'+dd;  
+        $('input[name=date]').val(today);
+        return true;
+    },
+    'click #yesterday': function(event) {
+        event.stopPropagation();
+        var today = new Date();
+        var yesterday = new Date(today);
+        yesterday.setDate(today.getDate() - 1);
+        var dd = yesterday.getDate();
+        var mm = yesterday.getMonth()+1;
+        var yyyy = yesterday.getFullYear();
+        if(dd<10){dd='0'+dd} 
+        if(mm<10){mm='0'+mm} 
+        yesterday = yyyy+'-'+mm+'-'+dd;  
+        $('input[name=date]').val(yesterday);
+        return true;
+    },  
+});
