b5167cb4127a062337a97e79f3d542552a09398b
Tommi Savikko <tommi.savikko@skydivejkl.fi>
2015-05-27 17:29:30 +0300

    first version of login system

--- client/views/navbar.js
+++ client/views/navbar.js
@@ -1,5 +1,13 @@
 Template.navbar.events({
     'click #logout': function(e,tmpl) {
-      Meteor.logout();
+    Meteor.logout(function (err, res) {
+      console.log('logout answered', arguments);
+      if(err)
+        Session.set('loginPasswordlessMessage', err.error);
+      else {
+        Session.set('loginPasswordlessMessage', '');
+        Session.set('loginPasswordlessState', 'loginPasswordlessLogin');
+      }
+    });
     }
 });
