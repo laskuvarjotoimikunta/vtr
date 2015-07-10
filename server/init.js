96256f5833b858e961945cc4972b9aeb877d8b32
Tommi Savikko <tommi.savikko@skydivejkl.fi>
2015-07-11 00:07:35 +0300

    moved admin-thingies to Meteor.startup

--- server/init.js
+++ server/init.js
@@ -1,24 +1,18 @@
-//Initialize upload-server
 Meteor.startup(function () {
-  UploadServer.init({
-    tmpDir: '/srv/vtr_uploads/tmp/',
-    uploadDir: '/srv/vtr_uploads/',
-    checkCreateDirectories: true //create the directories for you
-  })
+  // list people who are admins
+  adminemails = [
+    'jari.lehti@ilmailuliitto.fi',                // SIL lajipäällikkö
+    'johanna.huhtapelto@laskuvarjotoimikunta.fi', // LT pj
+    'henri.tyrvainen@laskuvarjotoimikunta.fi',    // LT vpj
+    'mari.lehtonen@laskuvarjotoimikunta.fi'       // LT turvallisuusvastaava
+    ];
+
+  // add everyone one adminemails to admin role
+  _.each(adminemails, function(email) { 
+    if (Meteor.users.findOne({ emails: {$elemMatch: { address: email } } })) // if user has already registered
+      {
+        var userId = Meteor.users.findOne({ emails: {$elemMatch: { address: email } } })._id;
+        Roles.addUsersToRoles(userId,['admin'], Roles.GLOBAL_GROUP);
+      }
+  });
 });
-
-adminemails = [
-  'jari.lehti@ilmailuliitto.fi',                // SIL lajipäällikkö
-  'johanna.huhtapelto@laskuvarjotoimikunta.fi', // LT pj
-  'henri.tyrvainen@laskuvarjotoimikunta.fi',    // LT vpj
-  'mari.lehtonen@laskuvarjotoimikunta.fi'       // LT turvallisuusvastaava
-  ];
-
-// add everyone one adminemails to admin role
-_.each(adminemails, function(email) { 
-  if (Meteor.users.findOne({ emails: {$elemMatch: { address: email } } })) // if user has already registered
-    {
-      var userId = Meteor.users.findOne({ emails: {$elemMatch: { address: email } } })._id;
-      Roles.addUsersToRoles(userId,['admin'], Roles.GLOBAL_GROUP);
-    }
-})
