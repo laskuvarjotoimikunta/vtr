39d9124d83e56fba1b6f808523f543f247150ae8
Tommi Savikko <tommi.savikko@skydivejkl.fi>
2015-07-10 11:31:35 +0300

    changed vtr sub to router config

--- client/_main.js
+++ client/_main.js
@@ -1,8 +1,5 @@
 Meteor.subscribe('dropzones');
 
-Meteor.subscribe('myVtrs');
-Meteor.subscribe('dzVtrs');
-
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') ) {
         $(this).collapse('hide');
