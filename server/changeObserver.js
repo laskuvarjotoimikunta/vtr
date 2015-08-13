ca24e097b39435dc2b429a3393900cadb2a4930f
Tommi Savikko <tommi.savikko@skydivejkl.fi>
2015-08-13 18:10:55 +0300

    removed observer from added (as it is unnecessary)

--- server/changeObserver.js
+++ server/changeObserver.js
@@ -1,7 +1,4 @@
 Vtr.find({}).observeChanges({
-    added : function (id, doc) {
-      console.log("ADDED: " + id, doc);
-    },
     changed : function (id, fields) {
       console.log("CHANGE: ", id, fields);
     }
