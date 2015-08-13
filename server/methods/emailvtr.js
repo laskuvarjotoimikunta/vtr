98718335a0c0bed4bcf6e0f9826296a8706e1397
Tommi Savikko <tommi.savikko@skydivejkl.fi>
2015-08-13 18:16:38 +0300

    added vtr subject to email notification and also changed ; to , as email address separator

--- server/methods/emailvtr.js
+++ server/methods/emailvtr.js
@@ -4,11 +4,11 @@ Meteor.methods({
     console.log('Starting to send email on vtr ' + newVtrDoc._id + ' user ' + this.userId + ' type ' + operation);
     console.log('Whole document: ' + JSON.stringify(newVtrDoc));
     if (operation==='update') { 
-        var emailSubject = 'Vaaratilanneilmoitusta muokattu';
+        var emailSubject = 'Vaaratilanneilmoitusta muokattu ' + newVtrDoc.subject;
         var emailBody = 'Vaaratilanneilmoitusta on muokattu.';
     }
     else {
-        var emailSubject = 'Uusi vaaratilanneilmoitus tehty';
+        var emailSubject = 'Uusi vaaratilanneilmoitus tehty ' + newVtrDoc.subject;
         var emailBody = 'Uusi vaaratilanneilmoitus on tehty.';
     };
     useremail =  Meteor.users.findOne(this.userId).emails[0].address;
@@ -24,7 +24,7 @@ Meteor.methods({
       from: "Vaaratilanneilmoitus <vtr@laskuvarjotoimikunta.fi>",
       to: "Vaaratilanneilmoitus <vtr@laskuvarjotoimikunta.fi>",
       bcc: useremail,
-      cc: ccs.join(";"),
+      cc: ccs.join(","),
       subject: emailSubject,
       text: emailBody + 
             "\n\n" +
